588cd84687e91bcde4c6b384bcc487b95c2e4ff4
FRJKNOEL <JKNOEL@users.noreply.github.com>
2019-05-12 19:49:17 +0400

    Pour Max

---
+++ pygameMenu/config_controls.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+# Import locals
+import pygame.locals as __locals
+
+# Controls
+MENU_CTRL_BACK = __locals.K_BACKSPACE
+MENU_CTRL_CLOSE_MENU = __locals.K_ESCAPE
+MENU_CTRL_DOWN = __locals.K_UP
+MENU_CTRL_ENTER = __locals.K_RETURN
+MENU_CTRL_LEFT = __locals.K_LEFT
+MENU_CTRL_RIGHT = __locals.K_RIGHT
+MENU_CTRL_UP = __locals.K_DOWN
