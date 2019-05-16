246a3333bcdf2e5fe3a4dd058a250a2004e2f1de
FRJKNOEL <JKNOEL@users.noreply.github.com>
2019-05-16 20:56:35 +0400

    version 2.1

---
+++ pygameMenu/__init__.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from pygameMenu.menu import Menu
+from pygameMenu.textmenu import TextMenu
+import pygameMenu.fonts as fonts
+
+# Other
+__author__ = 'LEVENEUR Jean-Kenny'
+__contributors__ = [
+    'LEVENEUR Jean-Kenny',
+    'K/BIDI Maxime',
+    'HUET Kévin',
+]
+__description__ = 'Menu pour pygame, simple'
+__email__ = 'jean.leveneur@hotmail.com'
+__url__ = 'https://github/FRJKNOEL/runnerz'
+__version__ = 'v1.96.1'
